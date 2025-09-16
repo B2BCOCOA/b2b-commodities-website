fbd303b3d6962eab8af73535f744749ac544eb63
Mohammed Kadri <mohkadri661@gmail.com>
2025-09-16 13:22:13 +0200

    Fixed favicon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import brofaFavicon from './assets/icons/nav_icon.png'
 
 // Import CSS styles
 import './assets/css/main.css'
@@ -14,4 +15,19 @@ app.mount('#app')
 // Ensure correct document title after mount (some hosts cache default)
 if (typeof document !== 'undefined') {
 	document.title = 'BROFA Investment'
+	// Update favicon to the requested asset from src/assets
+	const setFavicon = (href) => {
+		const ensure = (rel) => {
+			let link = document.querySelector(`link[rel='${rel}']`)
+			if (!link) {
+				link = document.createElement('link')
+				link.setAttribute('rel', rel)
+				document.head.appendChild(link)
+			}
+			link.setAttribute('href', href)
+		}
+		ensure('icon')
+		ensure('shortcut icon')
+	}
+	setFavicon(brofaFavicon)
 }
